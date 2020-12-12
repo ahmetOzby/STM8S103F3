a19457b75930b0487d0784353b3dbd6bb8183981
ahmetOzby <63069506+ahmetOzby@users.noreply.github.com>
2020-12-12 18:16:54 +0300

    Comments added.

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <STM8S103F3P.h>
 #include "stdint.h"
 
+//Delay function
 void myDelay(uint32_t value);
 
 
 main()
 {
-
-    PB_DDR |=0x20;// PB.5 as Output
-    PB_CR1 |=0x20;// PB.5 as Push Pull Type Output
+    //PB_DDR is port B data direction register address. PB5 pin set as output.
+    PB_DDR |=0x20;
+    //PB_CR1 is port B control register. PB5 pin set as push-pull output.
+    PB_CR1 |=0x20;
     while(1)
     {
+        //PB5 output set as high (logic 1).
         PB_ODR |=1<<5;// PB.5 = 1
         myDelay(500);
+	//PB5 output set as low (logic 0).
         PB_ODR &=~(1<<5);// PB.5 = 0
         myDelay(500);
     }
@@ -23,7 +27,7 @@ main()
 
 void myDelay(uint32_t value)
 {
- int i,j;
+ uint16_t i,j;
  for(i = 0; i < value; i++)
  {
 		for(j = 0 ; j < 100 ; j++);
